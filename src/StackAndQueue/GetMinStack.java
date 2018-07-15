49abb07425be143f2b88cf929f0b9ba3971324d0
lcg829 <lichenguang829@163.com>
2018-07-16 02:54:14 +0800

    backage commit

---
+++ src/StackAndQueue/GetMinStack.java
@@ -0,0 +1,12 @@
+package StackAndQueue;
+
+/**
+ * 设计一个有getMin功能的栈
+ * 1.pop, push, getMin操作的时间复杂度都是O(1)
+ * 2.设计的栈类型可以使用现成的栈结构
+ * 
+ * 设计两个栈，一个普通栈，一个存各个时期的最小值
+ */
+public class GetMinStack {
+	
+}
